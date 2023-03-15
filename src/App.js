11ae4577d73a994694fd88f71bbdb3b0a2697732
Ivan Yaremko <yaremich98@gmail.com>
2023-03-15 09:18:21 +0200

    (60) React. Подъем состояния

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import CiaoSection from './components/CiaoSection';
+import UsersList from './components/UsersList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const userDB = [
+  {
+  id:1,
+  firstName:'Elon'
+  },
+  {
+  id:2,
+  firstName:'Nazar'
+  },
+  {
+  id:3,
+  firstName:'Ann'
+  }
+];
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        users:userDB.map((user) => ({...user, isSelected:false}))
+    }
+}
+setUserSelector = (newUsers) => {this.
+setState({users:newUsers})}
+  render(){
+    const {users} = this.state;
+    return <>
+    {/* <CiaoSection /> */}
+    <header>
+      <SelectedUsersList users={users}  />
+    </header>
+    <main>
+      <UsersList users={users} setUserSelector={this.setUserSelector}/>
+    </main>
+   
+    </>
+  }
 }
 
 export default App;
---
+++ src/components/Ciao/index.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+class Ciao extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isGreeting:true
+        }
+    }
+    handlerBtn = () => {
+        const {isGreeting} = this.state;
+        this.setState({
+            isGreeting:isGreeting
+        })
+    }
+    render(){
+        const {name, id} = this.props;
+        const {isGreeting} = this.state;
+        if(isGreeting){
+            return <>
+            <h2>{id} Hi, {name}</h2>
+            <button onClick={this.handlerBtn}>Ciao</button>
+        </>
+        }
+        return <h2>Bye {name}!</h2>
+    }
+}
+export default Ciao;
